1ac7c774f0fb2dff815059099b17a330ff50a1f3
Tomasz Kawalec <tkawalec369@gmail.com>
2024-07-28 11:40:02 +0200

    Change syntax a bit (most notably remove semicolons) and continue working on tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod tokenizer;
+
+use clap;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    input_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Transpiling {}...", args.input_file);
+
+    let file_contents = std::fs::read_to_string(&args.input_file)
+        .expect(format!("Error occurred during reading file {}", &args.input_file).as_str());
+
+    let tokens = tokenizer::tokenize(file_contents.as_str()).unwrap();
+
+    println!("Tokens:\n{:?}", tokens);
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,42 +1,92 @@
+#[derive(Debug)]
 pub enum Keyword {
     Fn,
     Let,
+    LeftCurly,
+    RightCurly,
+    LeftParenthese,
+    RightParenthese,
+    Assign,
 }
 
+#[derive(Debug)]
 pub enum Token {
     Keyword(Keyword),
     Identifier(String),
     Number(f64),
+    String(String),
 }
 
-pub fn tokenize(code: &str) -> Vec<Token> {
-    let mut counter: u32 = 0;
+pub fn tokenize(code: &str) -> Result<Vec<Token>, String> {
     let mut result = Vec::<Token>::new();
-    while counter < code.len() {
-        match code[counter] {
-            letter if letter.is_alphabetic() || letter == '_' => {
+
+    let mut line_counter: usize = 1;
+    let mut column_counter: usize = 1;
+    //let mut prev_newline_char: Option<char> = None;
+    fn handle_newline(line_counter: &mut usize, column_counter: &mut usize) {
+        *line_counter += 1;
+        *column_counter = 1;
+    }
+
+    let mut char_iter = code.chars();
+    while let Some(ch) = char_iter.next() {
+        match ch {
+            letter if letter.is_alphabetic() || matches!(letter, '_' | '@') => {
                 //Either keyword or identifier
                 let mut identifier = String::new();
-                let mut ident_counter = counter + 1;
-                while ident_counter < code.len() {
-                    let ident_letter = code[ident_counter];
-                    if ident_letter.is_alphanumeric() || ident_letter == '_' {
-                        identifier.push(ident_letter);
+                identifier.push(letter);
+                let mut ident_len = 1;
+                let mut ident_char_iter = char_iter.clone();
+                for ident_ch in ident_char_iter {
+                    if ident_ch.is_alphanumeric() || ident_ch == '_' {
+                        identifier.push(ident_ch);
                     } else {
                         break;
                     }
+                    ident_len += 1;
                 }
 
-                let token = match identifier {
+                let token = match identifier.as_str() {
                     "fn" => Token::Keyword(Keyword::Fn),
                     "let" => Token::Keyword(Keyword::Let),
                     _ => Token::Identifier(identifier),
                 };
+                result.push(token);
 
-                counter = ident_counter - 1;
+                for _i in 0..(ident_len - 1) {
+                    char_iter.next();
+                }
             }
-        }
-        counter += 1;
+            '{' => {
+                result.push(Token::Keyword(Keyword::LeftCurly));
+            }
+            '}' => {
+                result.push(Token::Keyword(Keyword::RightCurly));
+            }
+            '(' => {
+                result.push(Token::Keyword(Keyword::LeftParenthese));
+            }
+            ')' => {
+                result.push(Token::Keyword(Keyword::RightParenthese));
+            }
+            '=' => {
+                result.push(Token::Keyword(Keyword::Assign));
+            }
+            ' ' | '\t' => {}
+            '\r' => { //Sorry macOS users, no line counting for you
+            }
+            '\n' => {
+                line_counter += 1;
+                column_counter = 1;
+            }
+            _ => {
+                return Err(format!(
+                    "Unexpected character '{}' at {}:{}",
+                    ch, line_counter, column_counter
+                ));
+            }
+        };
+        //counter += 1;
     }
-    result
+    Ok(result)
 }
